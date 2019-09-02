fe2fd6d2ddf029709bde8fe873b92b6da07b9b7e
zein rezky chandra <zeinrezky@gmail.com>
2019-09-02 08:44:10 +0700

    lil progress task list admin

---
+++ WeKiddo_Admin/Classes/Add New Task Admin/Add New Task Admin Cell/AddNewTaskAdminHeaderCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddNewTaskAdminHeaderCell.swift
+//  WeKiddo_Admin
+//
+//  Created by zein rezky chandra on 01/09/19.
+//  Copyright © 2019 PT. Absolute Connection. All rights reserved.
+//
+
+import UIKit
+
+class AddNewTaskAdminHeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
